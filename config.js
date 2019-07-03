594b09683946377c498b80b8ac0a9d46801863cb
Will McAuliff <womcauliff@gmail.com>
2019-07-02 23:17:48 -0400

    Adds GA tracking id

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-143236248-1',
   menu: [
     {
       label: 'Blog',
